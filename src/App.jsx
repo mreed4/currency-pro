6d3c905c08cd85761a75b3d4f794c73e95f126f6
Matthew <78713317+mreed4@users.noreply.github.com>
2023-10-22 14:01:56 -0700

    Netlify, router

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { Link, BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+import StartPage from "./StartPage";
 
+function Header() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <header>
+      <h1>Currencypro</h1>
+      <nav>
+        <ul className="main-nave">
+          <li>
+            <Link to="/currency-pro/">Currency Converter</Link>
+          </li>
+          <li>
+            <Link to="/currency-pro/">Features</Link>
+          </li>
+          <li>
+            <Link to="/currency-pro/">Resources</Link>
+          </li>
+        </ul>
+      </nav>
+      <nav className="secondary-nav">
+        <ul>
+          <li>
+            <Link to="/currency-pro/">Sign In</Link>
+          </li>
+          <li>
+            <Link to="/currency-pro/">Learn More</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
-export default App
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/currency-pro/" element={<StartPage />} />
+        </Routes>
+      </main>
+      <footer></footer>
+    </>
+  );
+}
+
+export default App;
---
+++ src/StartPage.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+export default function StartPage() {
+  return (
+    <div>
+      <h2>The safe and easy way to exchange your money</h2>
+      <p>
+        You always get the best exchange rate with Currencypro, whether you send, spend, or convert money in dozens of currencies. But don’t
+        take our word for it.
+      </p>
+      <h3>Select your base currency</h3>
+      <h3>Select your target currency</h3>
+    </div>
+  );
+}
